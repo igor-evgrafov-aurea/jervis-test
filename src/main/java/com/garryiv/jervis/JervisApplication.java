d6c6cd608f19ac10b456d262e74f95a74da02be6
Igor Evgrafov <igor.evgrafov@aurea.com>
2018-05-07 20:01:14 +0300

    Jervis

---
+++ src/main/java/com/garryiv/jervis/JervisApplication.java
@@ -0,0 +1,12 @@
+package com.garryiv.jervis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JervisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JervisApplication.class, args);
+	}
+}
